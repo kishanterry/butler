3676a5b0299500cce681439b5b670219b5f451e5
Amith Gotamey <kishanterry@gmail.com>
2015-04-17 12:55:34 +0600

    initial user

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -7,7 +7,16 @@ class UserTableSeeder extends Seeder {
 
     public function run()
     {
-        TestDummy::times(10)->create('Butler\Models\User');
+        // TestDummy::times(10)->create('Butler\Models\User');
+        DB::table('users')->truncate();
+
+        $user = [
+            'name' => 'Admin',
+            'email' => 'admin@abit.bt',
+            'password' => bcrypt('bhutan123'),
+        ];
+
+        DB::table('users')->insert($user);
     }
 
 }
